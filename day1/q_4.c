655f7237dfd36e1e919f9da810437e7d60506026
Riya-Chaudhary07 <rchaudhary6107@gmail.com>
2026-06-02 22:29:38 +0530

    d1q4

---
+++ day1/q_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int number,count=0;
+
+    printf("Enter a number:");
+    scanf("%d",&number);
+
+    if(number==0)
+    {
+        count=1;
+    }
+    else
+    {
+        while(number!=0)
+          {
+            number=number/10;
+            count++;
+          }    
+    }
+    
+     printf("Number of digits=%d",count);
+    
+    return 0;
+}
