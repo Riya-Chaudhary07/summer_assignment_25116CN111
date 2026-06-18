3d2b79b225532902debf3da8edd20b5f0bb1c22b
Riya-Chaudhary07 <rchaudhary6107@gmail.com>
2026-06-18 22:18:55 +0530

    d13q52

---
+++ day13/q_52.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+
+    for(i = 0; i < n; i++)
+
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+
+    {
+        if(arr[i] % 2 == 0)
+
+            even++;
+
+        else
+
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+
+    printf("Odd elements = %d\n", odd);
+
+    return 0;
+
+}
