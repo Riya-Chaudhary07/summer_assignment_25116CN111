95edd749eee65821ab47658d641a2f5818584322
Riya-Chaudhary07 <rchaudhary6107@gmail.com>
2026-06-27 09:30:36 +0530

    d16q61

---
+++ day16/q_61.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int a[100], n, i;
+    int sum = 0, total = 0, missing;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n - 1);
+
+    for(i = 0; i < n - 1; i++)
+
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    total = n * (n + 1) / 2;
+
+    missing = total - sum;
+
+    printf("Missing number is: %d", missing);
+
+    return 0;
+}
